8c8f6fe376bb263e4286a50085751e630f13866a
Cesar Manuel Cruzata De la Cruz <cruzatadelacruzc@gmail.com>
2020-05-12 11:46:45 -0400

    First Commit

---
+++ src/main/java/com/example/demo/config/ConfigServerConfig.java
@@ -0,0 +1,17 @@
+package com.example.demo.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@ConfigurationProperties(prefix = "spring.cloud.config.server")
+public class ConfigServerConfig {
+
+    private List<Map<String, Object>> composite = new ArrayList<>();
+
+    public List<Map<String, Object>> getComposite() {
+        return composite;
+    }
+}
---
+++ src/main/java/com/example/demo/config/info/CloudConfigInfoContributor.java
@@ -0,0 +1,27 @@
+package com.example.demo.config.info;
+
+import com.example.demo.config.ConfigServerConfig;
+import org.springframework.boot.actuate.info.Info;
+import org.springframework.boot.actuate.info.InfoContributor;
+import org.springframework.cloud.config.client.ConfigClientProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CloudConfigInfoContributor implements InfoContributor {
+
+    private final ConfigServerConfig configServerConfig;
+
+    private final ConfigClientProperties configClientProperties;
+
+    public CloudConfigInfoContributor(ConfigServerConfig configServerConfig,
+                                      ConfigClientProperties configClientProperties) {
+        this.configServerConfig = configServerConfig;
+        this.configClientProperties = configClientProperties;
+    }
+
+    @Override
+    public void contribute(Info.Builder builder) {
+        builder.withDetail("cloud-config-label", configClientProperties.getLabel());
+        builder.withDetail("cloud-config-server-configuration-sources", configServerConfig.getComposite());
+    }
+}
